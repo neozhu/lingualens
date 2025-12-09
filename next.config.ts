7f7172a28b3ec6410904421ccc2f26ad9117ad4d
neozhu <new163@163.com>
2025-12-09 17:00:54 +0800

    refactor: update matcher pattern in proxy.ts and change JSX setting in tsconfig.json

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-import type { NextConfig } from "next";
-
+import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
-const nextConfig: NextConfig = {
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
-};
+const nextConfig: NextConfig = {};
  
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
