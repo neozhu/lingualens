e15e8943bcefc24b3ae24582ea02e80e093b9ea7
neozhu <new163@163.com>
2025-12-09 14:37:42 +0800

    Add ESLint configuration to ignore builds

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 import createNextIntlPlugin from 'next-intl/plugin';
  
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+};
  
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
